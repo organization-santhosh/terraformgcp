90ed46adc74e4f07768f6a858e663f1f9aaf1f77
santhoshchintala <40984539+santhoshchintala@users.noreply.github.com>
2023-03-13 16:47:51 +0530

    Update main.tf

--- test-integration/main.tf
+++ test-integration/main.tf
@@ -7,8 +7,4 @@ resource "google_service_account_key" "mykey" {
   service_account_id = google_service_account.myaccount.name
 }
 
-resource "google_storage_bucket_object" "key" {
-  name   = "my-service-account-key.json"
-  bucket = "azuresanthu"
-  source = google_service_account_key.mykey.private_key
-}
+
