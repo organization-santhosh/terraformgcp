959269a9f4bde070fd843868b42fa254d9016218
santhoshchintala <40984539+santhoshchintala@users.noreply.github.com>
2023-03-13 15:45:34 +0530

    Update provider.tf

--- test-integration/provider.tf
+++ test-integration/provider.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.36.0"
+    }
+  }
+}
+
 provider "google" {
-  project = "dssrc-test-project-367514"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  # Configuration options
+  project = "stable-inn-369612"
+  region  = "europe-west1"
+  zone    = "europe-west1-a"
+
 }
